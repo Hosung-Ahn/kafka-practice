89788a5e9e4572194b88e2adda1b904f8385f52a
Hosung <an3735297@naver.com>
2024-01-02 13:33:23 +0900

    producer 세팅, 메시지 보내기

--- kafka/kafka-basic/src/main/java/io/conductor/demos/kafka/ProducerDemo.java
+++ kafka/kafka-basic/src/main/java/io/conductor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,41 @@
 package io.conductor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        log.info("Hello World!");
+
+        // set kafka producer properties
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+
+        props.setProperty("key.serializer", StringSerializer.class.getName());
+        props.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(record);
+
+        // flush data - send all data and block until complete
+        producer.flush();
+
+        // close producer
+        producer.close();
+
+
     }
 }
