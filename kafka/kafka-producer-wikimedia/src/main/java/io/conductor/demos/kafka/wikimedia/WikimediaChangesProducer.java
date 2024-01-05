f2b02325176763200a61262f3f06ce2e9fc3a60c
Hosung <an3735297@naver.com>
2024-01-06 00:38:13 +0900

    kafa-wiki-producer / producer 구현
    wikimedia 에서 실시간 스트림으로 제공하는 각 나라별 위키 업데이트 정보를 받아서 producer 에게 전달하는 객체입니다.

--- kafka/kafka-producer-wikimedia/src/main/java/io/conductor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka/kafka-producer-wikimedia/src/main/java/io/conductor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,44 @@
 package io.conductor.demos.kafka.wikimedia;
 
-public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
+import java.util.Properties;
+
+public class WikimediaChangesProducer {
+    private static final String bootstrapServers = "localhost:9092";
+    private static final String topic = "wikimedia.recentchange";
+    private static final String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", bootstrapServers);
+        props.setProperty("key.serializer", StringSerializer.class.getName());
+        props.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        Flux<String> stream = WebClient.create(url).get().retrieve().bodyToFlux(String.class);
+
+        stream.subscribe(
+                data -> {
+                    System.out.println(data);
+                    producer.send(
+                            new ProducerRecord<>(topic, data)
+                    );
+                },
+                error -> System.out.println("Error receiving data: " + error),
+                () -> System.out.println("Completed!!!")
+        );
+
+        // produce for 10 seconds
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
