51c2c697f651724b57af7ad15805003dd08d2b74
Hosung <an3735297@naver.com>
2024-01-12 18:53:25 +0900

    kafka-wiki-producer / 프로듀서 최적화
    batch size 조정, linger.ms (배치를 채우기 위해 기다리는 최대 시간) 조정, text 메세지에 적절한 압축 방법

--- kafka/kafka-producer-wikimedia/src/main/java/io/conductor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka/kafka-producer-wikimedia/src/main/java/io/conductor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,6 +1,7 @@
 package io.conductor.demos.kafka.wikimedia;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,11 +14,17 @@ public class WikimediaChangesProducer {
     private static final String topic = "wikimedia.recentchange";
     private static final String url = "https://stream.wikimedia.org/v2/stream/recentchange";
 
+
+
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", bootstrapServers);
         props.setProperty("key.serializer", StringSerializer.class.getName());
         props.setProperty("value.serializer", StringSerializer.class.getName());
+        // set high throughput producer config
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32 KB batch size
+        props.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
